4fe0df138ff57586fdf195a9e6f98baca35c066e
Snuggs <rstovall@innovative-studios.com>
2010-05-04 16:41:23 -0400

    Acceptance test /step_definitions and /support

---
+++ features/step_definitions/post_step.rb
@@ -0,0 +1,15 @@
+﻿Given /^I have started the Snuggsi application$/ do
+#  pending # express the regexp above with the code you wish you had
+end
+
+When /^I create content that inherits from thingi$/ do
+#  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I can post the content$/ do
+#  pending # express the regexp above with the code you wish you had
+end
+
+Then /^content is now accessible to the public$/ do
+#  pending # express the regexp above with the code you wish you had
+end
