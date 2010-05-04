f3cca1cf4b220385346d886556e8247136b6c9d8
Snuggs <rstovall@innovative-studios.com>
2010-05-04 17:21:37 -0400

    added post unit test /test/unit/post_test.rb

---
+++ test/unit/post_test.rb
@@ -0,0 +1,17 @@
+#require 'test_helper'
+require 'test/unit'
+
+class PostTest < Test::Unit::TestCase # ActiveSupport::TestCase
+  def setup
+  
+  end
+  
+  def teardown
+  
+  end
+  
+  test 'the truth' do
+    assert true # limit title
+    assert true # limit author
+  end
+end
